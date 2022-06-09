3da85f3120d811841243f7ab2628870630af794a
Felipe <felipecosta1961@gmail.com>
2022-06-09 15:13:26 -0300

    This commit changes the db config.

--- config/db.js
+++ config/db.js
@@ -5,14 +5,13 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const db = new Pool({
-  connectionString: process.env.DATABASE_URL
-});
-
-if (process.env.MODE === "PROD") {
-  db.ssl = {
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
     rejectUnauthorized: false
-  };
-}
+  }
+};
+
+const db = new Pool(databaseConfig);
 
 export default db;
